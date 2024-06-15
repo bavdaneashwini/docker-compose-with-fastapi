659ba59b44ec0421d3982956efc80966e4315523
ashwinibavdane <ashwini.bavdane@mirraw.com>
2024-06-15 09:23:05 +0530

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import redis
+import debugpy
+
+app = FastAPI()
+
+r = redis.Redis(host='redis',port=6379)
+# debugpy.listen('0.0.0.0',5678)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Ashwini"}
+
+
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"number of hits": r.get("hits")}
